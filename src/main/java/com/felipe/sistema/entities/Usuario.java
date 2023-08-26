e3ecc7c2c3272dda897251b6780adf4b3a0c3402
Felipe Santana <felipe012santana012@gmail.com>
2023-08-26 11:46:28 -0300

    Classe Pedido, Parte 01

--- src/main/java/com/felipe/sistema/entities/Usuario.java
+++ src/main/java/com/felipe/sistema/entities/Usuario.java
@@ -1,12 +1,15 @@
 package com.felipe.sistema.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +25,9 @@ public class Usuario implements Serializable {
 	private String email;
 	private String fone;
 	private String senha;
+	
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Usuario() {
 		
@@ -74,6 +80,10 @@ public class Usuario implements Serializable {
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -91,6 +101,8 @@ public class Usuario implements Serializable {
 		Usuario other = (Usuario) obj;
 		return Objects.equals(id, other.id);
 	}
+
+	
 	
 	
 	
